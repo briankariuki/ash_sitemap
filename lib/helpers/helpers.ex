62fe44a68669285d63048c71f5b08e04c1a72fc6
Brian Kariuki <briankariukidev@gmail.com>
2025-07-15 19:47:25 +0300

    update sitemap url headers

--- lib/helpers/helpers.ex
+++ lib/helpers/helpers.ex
@@ -210,7 +210,7 @@ defmodule AshSitemap.Helpers do
   """
   def schemas do
     %{
-      geo: "http://www.google.com/geo/schemas/sitemap/1.0",
+      geo: "http://www.google.com/geo/schemas/sitemap-geo/1.0",
       news: "http://www.google.com/schemas/sitemap-news/0.9",
       image: "http://www.google.com/schemas/sitemap-image/1.1",
       video: "http://www.google.com/schemas/sitemap-video/1.1",
@@ -230,11 +230,9 @@ defmodule AshSitemap.Helpers do
       xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
         http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
       xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'
-      xmlns:geo='http://www.google.com/geo/schemas/sitemap/1.0'
       xmlns:news='http://www.google.com/schemas/sitemap-news/0.9'
       xmlns:image='http://www.google.com/schemas/sitemap-image/1.1'
       xmlns:video='http://www.google.com/schemas/sitemap-video/1.1'
-      xmlns:mobile='http://www.google.com/schemas/sitemap-mobile/1.0'
       xmlns:pagemap='http://www.google.com/schemas/sitemap-pagemap/1.0'
       xmlns:xhtml='http://www.w3.org/1999/xhtml'
     >
@@ -269,4 +267,23 @@ defmodule AshSitemap.Helpers do
   def xml_index_footer do
     "</sitemapindex>"
   end
+
+  # def old_xml_header do
+  #   """
+  #   <?xml version="1.0" encoding="UTF-8"?>
+  #   <urlset
+  #     xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+  #     xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+  #       http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+  #     xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'
+  #     xmlns:geo='http://www.google.com/geo/schemas/sitemap/1.0'
+  #     xmlns:news='http://www.google.com/schemas/sitemap-news/0.9'
+  #     xmlns:image='http://www.google.com/schemas/sitemap-image/1.1'
+  #     xmlns:video='http://www.google.com/schemas/sitemap-video/1.1'
+  #     xmlns:mobile='http://www.google.com/schemas/sitemap-mobile/1.0'
+  #     xmlns:pagemap='http://www.google.com/schemas/sitemap-pagemap/1.0'
+  #     xmlns:xhtml='http://www.w3.org/1999/xhtml'
+  #   >
+  #   """
+  # end
 end
